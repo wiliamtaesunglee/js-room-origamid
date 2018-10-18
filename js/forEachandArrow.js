5e4eca79a08a12e67676fcf0103dc2a6b68b7187
wiliamtaesunglee <wiliam.tae@gmail.com>
2018-10-18 18:22:01 -0300

    forEach exercises

---
+++ js/forEachandArrow.js
@@ -0,0 +1,46 @@
+// const imgs = document.querySelectorAll('img');
+
+// let i = 0;
+// imgs.forEach(function(item, index, array) {
+//     console.log(i++);
+//     console.log(item, index);
+    
+// });
+
+// const titulos = document.getElementsByClassName('titulo');
+//!transforma "'titulos' em array"
+// const titulosArray = Array.from(titulos); 
+
+// titulosArray.forEach(function(item) {
+//     console.log(item);
+// });
+
+// const imgs = document.querySelectorAll('img');
+
+// imgs.forEach((item) => {
+//     console.log(item);
+// });
+
+// const paragrafoSite = document.querySelectorAll('p');
+// paragrafoSite.forEach((item) => {
+//     console.log(item);
+// });
+
+// const txtParagrafo = document.querySelectorAll('p');
+// txtParagrafo.forEach((item) => {
+//         console.log(item.innerText);
+// });
+
+
+// const imgs = document.querySelectorAll('img');
+
+// imgs.forEach((item, index) => {
+//     console.log(item, index);
+// });
+
+// let i = 0;
+// imgs.forEach(() => {
+//     console.log(i++)
+// });
+
+// imgs.forEach(() => i++)
---
+++ js/script.js
@@ -0,0 +1,30 @@
+    // const menu = document.querySelector('.menu');
+    // console.log(menu.classList);
+
+    // menu.classList.add('data-texto', 'teste');
+    // menu.classList.remove('ativo');
+    // menu.classList.remove("azul");
+    // menu.classList.toggle('azul');
+
+    // if (menu.classList.contains('azul')) {
+    //     menu.classList.add('possui-azul');
+    // } else {
+    //     menu.classList.add('nao-possui-azul');
+    // }
+
+    // menu.className = 'testes';
+
+    // const animais = document.querySelector('.animais');
+    //     console.log(animais.attributes[1]);
+    //     console.log(animais.attributes['data-texto']);
+
+    // const img = document.querySelector('img');
+    //! adiciona o atributo "alt" com o nome "Raposa"
+    // img.setAttribute('alt', 'Raposa');
+    // const srcImg = img.getAttribute('alt');
+    // console.log(srcImg);
+
+    menu.classList.add('teste');
+    
+    
+        
