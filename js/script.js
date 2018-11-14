efa1467cda46c039e050620c13f93fe2de51eb6f
wiliamtaesunglee <wiliam.tae@gmail.com>
2018-11-14 18:04:41 -0200

    first interacting adcinted

--- js/script.js
+++ js/script.js
@@ -1,30 +1,15 @@
-    // const menu = document.querySelector('.menu');
-    // console.log(menu.classList);
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-    // menu.classList.add('data-texto', 'teste');
-    // menu.classList.remove('ativo');
-    // menu.classList.remove("azul");
-    // menu.classList.toggle('azul');
+function activeTab(index) {
+    tabContent.forEach((section) => {
+        section.classList.remove('ativo');
+    })
+    tabContent[index].classList.add('ativo');
+}
 
-    // if (menu.classList.contains('azul')) {
-    //     menu.classList.add('possui-azul');
-    // } else {
-    //     menu.classList.add('nao-possui-azul');
-    // }
-
-    // menu.className = 'testes';
-
-    // const animais = document.querySelector('.animais');
-    //     console.log(animais.attributes[1]);
-    //     console.log(animais.attributes['data-texto']);
-
-    // const img = document.querySelector('img');
-    //! adiciona o atributo "alt" com o nome "Raposa"
-    // img.setAttribute('alt', 'Raposa');
-    // const srcImg = img.getAttribute('alt');
-    // console.log(srcImg);
-
-    // menu.classList.add('teste');
-    
-    
-        
+tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener('click', () => {
+        activeTab(index); 
+    })
+});
