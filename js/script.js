c2459a2c759b534ad69a047cac97586a79878493
wiliamtaesunglee <wiliam.tae@gmail.com>
2018-11-15 17:28:50 -0200

    animation soft scroll

--- js/script.js
+++ js/script.js
@@ -46,3 +46,27 @@ function initAccordion() {
 
 
 initAccordion();
+
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
+
+
+function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    const topo = section.offsetTop;
+
+    section.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+    });
+    //forma alternativa
+    // window.scrollTo({
+    //     top: topo,
+    //     behavior: 'smooth',
+    // })
+}
+
+linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+})
