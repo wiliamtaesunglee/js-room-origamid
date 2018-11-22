e9a97e01df1bb7e7163489e966202c1f5382b293
wiliamtaesunglee <wiliam.tae@gmail.com>
2018-11-22 11:02:32 -0200

    mudules exportation

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -7,7 +7,7 @@ export default function initScrollSuave() {
         event.preventDefault();
         const href = event.currentTarget.getAttribute('href');
         const section = document.querySelector(href);
-        const topo = section.offsetTop;
+        // const topo = section.offsetTop;
 
         section.scrollIntoView({
             behavior: 'smooth',
