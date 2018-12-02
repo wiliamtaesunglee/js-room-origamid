bd9aa70659fffdc7d7ebf611fc0a6c07c5468779
Wil.i.am <wiliam.tae@gmail.com>
2018-12-02 10:15:32 -0200

    dopdown and outsideclick adicted

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,17 +1,21 @@
+import outsideClick from './outsideclick.js';
+
 export default function initDropDownMenu() {
+  const dropDownMenu = document.querySelectorAll('[data-dropdown]');
+  dropDownMenu.forEach(menu => {
+    ['touchstart', 'click'].forEach(userEvent => {
+    menu.addEventListener(userEvent, handleClick);
+    });
+  });
 
+  function handleClick(event) {
+    event.preventDefault();
+    this.classList.add('active');
+    outsideClick(this, ['touchstart', 'click'], () => {
+      this.classList.remove('active');
+    });
+  };
 }
 
-const dropDownMenu = document.querySelectorAll('[data-dropdown]');
 
-dropDownMenu.forEach(menu => {
-  ['touchstart', 'click'].forEach(userEvent => {
-  menu.addEventListener(userEvent, handleClick)
 
-  })
-});
-
-function handleClick(event) {
-  event.preventDefault();
-  this.classList.toggle('active');
-}
