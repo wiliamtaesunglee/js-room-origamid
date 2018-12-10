eaba510710e5d1f8f0e05a40b01c06d3f9e13dbb
wiliamtaesunglee <wiliam.tae@gmail.com>
2018-12-10 19:25:10 -0200

    fetch adicted

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ import initDropDownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initAnimaNumeros from './modules/anima-numeros.js';
 import initFuncionamento from './modules/funcionamento.js';
+import initFetchAnimais from './modules/fetch-animais.js';
 
 initAccordion();
 initScrollSuave();
@@ -19,3 +20,4 @@ initDropDownMenu();
 initMenuMobile();
 initAnimaNumeros();
 initFuncionamento();
+initFetchAnimais();
